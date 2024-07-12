86732f1e3bdd7f915377aeb476b1fde6111c1abc
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-07-12 20:21:25 +0300

    get All categories

--- server/services/categoryService.js
+++ server/services/categoryService.js
@@ -2,16 +2,17 @@ const CategoryModel = require("../models/categoryModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 
-exports.getCategories = (req, res) => {
-  // const name = req.body.name;
 
-  // console.log(req.body);
-  res.send();
-};
+// @route GET /api/v1/categories
 
+exports.getCategories = asyncHandler(async (req, res) => {
+  const categories = await CategoryModel.find({});
+  res.status(200).json({results: categories.length, data: categories});
+});
+
+// @route POST /api/v1/categories
 exports.craeteCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
-
   const category = await CategoryModel.create({ name, slug: slugify(name) });
   res.status(201).json({ data: category });
 });
