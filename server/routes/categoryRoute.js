4ba5c6e7b0528198c4aec935fc711b179bf47cc6
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-07-12 20:39:56 +0300

    GET Specific Category

--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -3,10 +3,12 @@ const express = require("express");
 const {
   getCategories,
   craeteCategory,
+  getCategory,
 } = require("../services/categoryService");
 
 const router = express.Router();
 
 router.route("/").get(getCategories).post(craeteCategory);
+router.route("/:id").get(getCategory);
 
 module.exports = router;
--- server/services/categoryService.js
+++ server/services/categoryService.js
@@ -13,6 +13,17 @@ exports.getCategories = asyncHandler(async (req, res) => {
   res.status(200).json({ results: categories.length, page, data: categories });
 });
 
+
+// @route GET /api/v1/categories/:id
+exports.getCategory = asyncHandler(async(req, res) => {
+  const {id} = req.params;
+  const category = await CategoryModel.findById(id);
+  if(!category){
+    res.status(404).json({msg:`No category for this id ${id}` });
+  }
+  res.status(200).json({data: category});
+})
+
 // @route POST /api/v1/categories
 exports.craeteCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
