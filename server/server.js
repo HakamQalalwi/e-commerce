caa06fb33d5fe07b451ffd5e25b252f31884c0f2
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-08-06 01:56:16 +0300

    Add Product To Wishlist Handler And Route

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const brandRouter = require("./routes/brandRoute");
 const userRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute");
 const reviewRoute = require("./routes/reviewRoute");
+const wishlistRoute = require("./routes/wishlistRoute");
 
 dbConnection();
 // express app
@@ -35,6 +36,7 @@ app.use("/api/v1/products", productRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/reviews", reviewRoute);
+app.use("/api/v1/wishlist", wishlistRoute);
 
 app.all("*", (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
---
+++ server/services/wishlistService.js
@@ -0,0 +1,21 @@
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/apiError");
+const User = require("../models/userModel");
+
+exports.addProductToWishlist = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $addToSet: { wishlist: req.body.productId },
+    },
+    { new: true }
+  );
+
+  res
+    .status(200)
+    .json({
+      status: "success",
+      message: "Product added successfully to your wishlist.",
+      data: user.wishlist,
+    });
+});
