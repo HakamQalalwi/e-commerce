b7e2552c0b08a6517fcb63a0697acbb886512173
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-08-07 00:29:12 +0300

    Refactor Mount Routes Middleware

--- server/server.js
+++ server/server.js
@@ -8,16 +8,18 @@ const ApiError = require("./utils/apiError");
 const dbConnection = require("./config/database");
 const globalError = require("./middleware/errorMiddleware");
 
-const categoryRoute = require("./routes/categoryRoute");
-const subCategoryRoute = require("./routes/subCategoryRoute");
-const productRoute = require("./routes/productRoute");
-const brandRouter = require("./routes/brandRoute");
-const userRoute = require("./routes/userRoute");
-const authRoute = require("./routes/authRoute");
-const reviewRoute = require("./routes/reviewRoute");
-const wishlistRoute = require("./routes/wishlistRoute");
-const addressRoute = require("./routes/addressRoute");
-const couponRoute = require("./routes/couponRoute");
+
+const mountRoutes = require("./routes")
+// const categoryRoute = require("./routes/categoryRoute");
+// const subCategoryRoute = require("./routes/subCategoryRoute");
+// const productRoute = require("./routes/productRoute");
+// const brandRouter = require("./routes/brandRoute");
+// const userRoute = require("./routes/userRoute");
+// const authRoute = require("./routes/authRoute");
+// const reviewRoute = require("./routes/reviewRoute");
+// const wishlistRoute = require("./routes/wishlistRoute");
+// const addressRoute = require("./routes/addressRoute");
+// const couponRoute = require("./routes/couponRoute");
 
 dbConnection();
 // express app
@@ -31,16 +33,18 @@ if (process.env.NODE_ENV === "development") {
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-app.use("/api/v1/categories", categoryRoute);
-app.use("/api/v1/subcategories", subCategoryRoute);
-app.use("/api/v1/brands", brandRouter);
-app.use("/api/v1/products", productRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/wishlist", wishlistRoute);
-app.use("/api/v1/addresses", addressRoute);
-app.use("/api/v1/coupons", couponRoute);
+
+mountRoutes(app);
+// app.use("/api/v1/categories", categoryRoute);
+// app.use("/api/v1/subcategories", subCategoryRoute);
+// app.use("/api/v1/brands", brandRouter);
+// app.use("/api/v1/products", productRoute);
+// app.use("/api/v1/users", userRoute);
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/reviews", reviewRoute);
+// app.use("/api/v1/wishlist", wishlistRoute);
+// app.use("/api/v1/addresses", addressRoute);
+// app.use("/api/v1/coupons", couponRoute);
 
 app.all("*", (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
