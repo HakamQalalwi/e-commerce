b1c25a1446425de08193f99358fb5a44c008ded7
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-08-08 02:51:04 +0300

    create cash Order service

---
+++ server/services/orderService.js
@@ -0,0 +1,49 @@
+const asyncHandler = require("express-async-handler");
+const factory = require("./handlersFactory");
+const ApiError = require("../utils/apiError");
+
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const Cart = require("../models/cartModel");
+const Order = require("../models/orderModel");
+
+// @route   POST /api/v1/orders/cartId
+// @access  Protected/User
+exports.createCashOrder = asyncHandler(async (req, res, next) => {
+  const taxPrice = 0;
+  const shippingPrice = 0;
+
+  const cart = await Cart.findById(req.params.cartId);
+  if (!cart) {
+    return next(
+      new ApiError(`There is no such cart with id ${req.params.cartId}`, 404)
+    );
+  }
+
+  const cartPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount
+    : cart.totalCartPrice;
+
+  const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+
+  const order = await Order.create({
+    user: req.user._id,
+    cartItems: cart.cartItems,
+    shippingAddress: req.body.shippingAddress,
+    totalOrderPrice,
+  });
+
+  if (order) {
+    const bulkOption = cart.cartItems.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product },
+        update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+      },
+    }));
+    await Product.bulkWrite(bulkOption, {});
+
+    await Cart.findByIdAndDelete(req.params.cartId);
+  }
+
+  res.status(201).json({ status: "success", data: order });
+});
