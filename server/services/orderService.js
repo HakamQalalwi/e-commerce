855d9f6a9062fb2746c55d926581f7dfc393cfc1
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-08-17 02:14:26 +0300

    add webhook

--- server/services/orderService.js
+++ server/services/orderService.js
@@ -201,7 +201,7 @@ exports.webhookCheckout = asyncHandler(async (req, res, next) => {
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-  if (event.type === checkout.session.completed) {
+  if (event.type === "checkout.session.completed") {
     createCashOrder(event.data.object);
   }
 
